20f3cbd375ff6c58ae4250d75e3a14ab91a0f1ff
rakshitvirmani <rickyvirmani@gmail.com>
2023-12-16 11:33:40 +0530

    Updated hard to-do

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,48 @@
 */
 
 class Todo {
+  constructor(){
+    this.listOfToDos = []
+  }
 
+  add(toDoText){
+    this.listOfToDos.push(toDoText);
+    return this.listOfToDos;
+  }
+
+  remove(toDoIndex){
+    if (toDoIndex > -1) { 
+      this.listOfToDos.splice(toDoIndex, 1);
+      return this.listOfToDos;
+    }
+  }
+
+  update(toDoIndex, toDoText){
+    this.listOfToDos[toDoIndex] = toDoText;
+    return this.listOfToDos;
+  }
+
+  getAll(){
+    return this.listOfToDos;
+  }
+
+  get(toDoIndex){
+    return this.listOfToDos[toDoIndex]
+  }
+
+  clear(){
+    this.listOfToDos = [];
+    return this.listOfToDos;
+  }
 }
 
-module.exports = Todo;
+//module.exports = Todo;
+const todoList = new Todo()
+todoList.add('Task 1');
+todoList.add('Task 2');
+todoList.add('Task 3');
+
+todoList.update(1, 'Updated Task 2');
+console.log(todoList.get(1))
+todoList.update(3, 'Invalid Task')
+console.log(todoList.getAll())
